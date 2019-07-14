cea2f128bb5c716e08e05056b00b27772a520ff9
Billy Leitch <wlei6277@gmail.com>
2019-07-14 21:30:04 +1000

    Setup redux and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import {connect} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route  
+            exact path="/"
+            component={Home}
+          />
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    
+  };
+}
+
+export default connect(mapStateToProps)(App);
